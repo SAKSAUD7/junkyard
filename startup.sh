171a25f9418fbd78948f9b9af4dc0648dac3755f
SAKSAUD7 <saksaud7411@gmail.com>
2026-02-13 18:28:51 +0530

    [backend] Fix startup.sh path for Azure deployment

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 # Azure App Service startup script for Django application
 
 # Navigate to backend directory
-cd /tmp/8de6a191c3f5482/backend
+cd /home/site/wwwroot/backend
 
 # Run database migrations
 python manage.py migrate --noinput
@@ -10,7 +10,6 @@ python manage.py migrate --noinput
 # Collect static files
 python manage.py collectstatic --noinput
 
-# Start Gunicorn
 # Start Gunicorn
 gunicorn --bind=0.0.0.0:8000 --timeout 600 --workers 4 core.wsgi:application
 
