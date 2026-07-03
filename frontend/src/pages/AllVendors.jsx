87d03152cb58ae7daedb00311bf6349907b4fde5
SAKSAUD7 <saksaud7411@gmail.com>
2026-07-03 18:36:48 +0530

    fix: corrected missing return statement in AllVendors map loop causing cards not to render

--- frontend/src/pages/AllVendors.jsx
+++ frontend/src/pages/AllVendors.jsx
@@ -220,11 +220,11 @@ const AllVendors = () => {
                 {/* Vendor Grid */}
                 {!loading && !error && vendors.length > 0 && (
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-                        {vendors.map((vendor, index) => {
-                                <div key={vendor.id} className="h-full">
-                                    <VendorCard vendor={vendor} />
-                                </div>
-                        })}
+                        {vendors.map((vendor, index) => (
+                            <div key={vendor.id} className="h-full">
+                                <VendorCard vendor={vendor} />
+                            </div>
+                        ))}
                     </div>
                 )}
 
